256b3b406d0a44e59522b5d4edd38f6aceadf22e
Harsh Raj Singh <singhharsh199@gmail.com>
2026-07-16 23:40:28 +0100

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) return false;
+        int[] s1_freq = new int[26];
+        int[] window_freq = new int[26];
+        for(int i = 0; i < s1.length(); i++){
+            s1_freq[s1.charAt(i) - 'a']++;
+            window_freq[s2.charAt(i) - 'a']++;
+        }
+        if(matches(s1_freq,window_freq)) return true;
+        for(int right = s1.length(); right < s2.length(); right++){
+            window_freq[s2.charAt(right) - 'a']++;
+            window_freq[s2.charAt(right - s1.length()) - 'a']--;
+            if(matches(s1_freq,window_freq)) return true;
+        }
+        return false;
+    }
+    public boolean matches(int[] arr1, int[] arr2){
+        for(int i = 0; i < 26; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+}
